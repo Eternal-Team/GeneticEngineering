25ee9a82a73b1e47ebb7c4c0155167e61fe13728
Itorius <itorius.yt@gmail.com>
2020-02-23 15:53:15 +0100

    Syringe sprite and tooltip

--- Items/Syringe.cs
+++ Items/Syringe.cs
@@ -1,7 +1,11 @@
 ﻿using BaseLibrary;
 using BaseLibrary.Items;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 using System;
+using System.Collections.Generic;
 using Terraria;
+using Terraria.DataStructures;
 using Terraria.ID;
 using Terraria.ModLoader;
 using Terraria.ModLoader.IO;
@@ -11,6 +15,8 @@ namespace GeneticEngineering.Items
 	public class Syringe : BaseItem
 	{
 		public override bool CloneNewInstances => false;
+
+		public override string Texture => "GeneticEngineering/Textures/Items/Syringe";
 
 		public int? npcType;
 
@@ -39,12 +45,17 @@ namespace GeneticEngineering.Items
 			return copy;
 		}
 
+		public override void SetStaticDefaults()
+		{
+			Main.RegisterItemAnimation(item.type, new DrawAnimationVertical(60, 2));
+		}
+
 		public override void SetDefaults()
 		{
 			item.damage = 2;
 			item.melee = true;
-			item.width = 40;
-			item.height = 40;
+			item.width = 28;
+			item.height = 28;
 			item.useTime = 20;
 			item.useAnimation = 20;
 			item.knockBack = 1;
@@ -55,11 +66,23 @@ namespace GeneticEngineering.Items
 			item.useStyle = ItemUseStyleID.Stabbing;
 		}
 
+		public override bool PreDrawInInventory(SpriteBatch spriteBatch, Vector2 position, Rectangle frame, Color drawColor, Color itemColor, Vector2 origin, float scale)
+		{
+			Texture2D texture = ModContent.GetTexture(Texture);
+
+			spriteBatch.Draw(texture, position, npcType == null ? new Rectangle(0, 0, 28, 28) : new Rectangle(0, 28, 28, 28), drawColor, 0f, origin, scale, SpriteEffects.None, 0f);
+
+			return false;
+		}
+
+		public override void ModifyTooltips(List<TooltipLine> tooltips)
+		{
+			if (npcType != null) tooltips.Add(new TooltipLine(mod, "BloodType", $"Currently holding the blood of {Utility.Cache.NPCCache[npcType.Value].GivenOrTypeName}"));
+		}
+
 		public override void OnHitNPC(Player player, NPC target, int damage, float knockBack, bool crit)
 		{
 			if (npcType == null) npcType = target.type;
-
-			Main.NewText(npcType.Value);
 		}
 
 		public override bool CanRightClick() => true;
